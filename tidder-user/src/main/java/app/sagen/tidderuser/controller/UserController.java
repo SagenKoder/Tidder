4f887e5a2604e5fac29d79398b9907dc7f16ede4
Alexander Sagen <alexmsagen@gmail.com>
2019-04-03 21:17:27 +0200

    Added <a>

--- tidder-user/src/main/java/app/sagen/tidderuser/controller/UserController.java
+++ tidder-user/src/main/java/app/sagen/tidderuser/controller/UserController.java
@@ -17,8 +17,8 @@ public class UserController {
 
     @RequestMapping("/")
     public String home() {
-        String response = "Welcome to the User Service!\n";
-        response += "I am assigned the following network interfaces:\n";
+        String response = "<a>Welcome to the User Service!</a>\n";
+        response += "<a>I am assigned the following network interfaces:</a>\n";
         try {
             Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
             while(networkInterfaces.hasMoreElements()) {
@@ -29,16 +29,16 @@ public class UserController {
                 while(inetAddresses.hasMoreElements()) {
                     num++;
                     InetAddress inetAddress = inetAddresses.nextElement();
-                    inetData += "    - " + inetAddress.getHostAddress() + "\n";
+                    inetData += "    - " + inetAddress.getHostAddress() + "</a>\n";
                 }
                 if(num > 0) response += inetData;
             }
         } catch (Exception e) {
-            response += "Exception while getting interface information! Error: " + e.getMessage();
+            response += "<a>Exception while getting interface information! Error: " + e.getMessage();
             e.printStackTrace();
         }
-        response += "Listening to port: " + serverPort + "\n";
-        response += "Server time: " + new Date().toString() + "\n";
+        response += "<a>Listening to port: " + serverPort + "</a>\n";
+        response += "<a>Server time: " + new Date().toString() + "</a>\n";
 
         return response.replace("\n", "<br/>\n");
     }
