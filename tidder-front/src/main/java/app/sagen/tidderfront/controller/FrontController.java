262d4ce8eda10713636991503a7997d3b84733cf
Alexander Sagen <alexmsagen@gmail.com>
2019-04-10 20:57:53 +0200

    Fixed redirect

--- tidder-front/src/main/java/app/sagen/tidderfront/controller/FrontController.java
+++ tidder-front/src/main/java/app/sagen/tidderfront/controller/FrontController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 @RequestMapping("/t")
 public class FrontController {
@@ -21,10 +23,17 @@ public class FrontController {
     }
 
     @GetMapping("{topic}")
-    public @ResponseBody String topic(Model model, @PathVariable("topic") String topic) {
+    public @ResponseBody String topic(HttpServletResponse response, Model model, @PathVariable("topic") String topic) {
         String filteredTopic = topic.toLowerCase();
         filteredTopic = filteredTopic.replaceAll("[^a-z0-9]", "");
-        if(!filteredTopic.equals(topic)) return "redirect:/t/" + filteredTopic;
+        if(!filteredTopic.equals(topic)) {
+            try {
+                response.sendRedirect("/t/" + filteredTopic);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
         return "This is the topic site for " + topic;
     }
 
