0d1d8b71be4d70d89fea8c116a7b8a2fa472d0db
Alexander Sagen <alexmsagen@gmail.com>
2019-04-10 20:51:48 +0200

    Filtered topic names

--- tidder-front/src/main/java/app/sagen/tidderfront/controller/FrontController.java
+++ tidder-front/src/main/java/app/sagen/tidderfront/controller/FrontController.java
@@ -15,11 +15,16 @@ public class FrontController {
 
     @PostMapping()
     public String topic(@RequestParam String topic) {
+        topic = topic.toLowerCase();
+        topic = topic.replaceAll("[^a-z0-9]", "");
         return "redirect:/t/" + topic;
     }
 
     @GetMapping("{topic}")
     public @ResponseBody String topic(Model model, @PathVariable("topic") String topic) {
+        String filteredTopic = topic.toLowerCase();
+        filteredTopic = filteredTopic.replaceAll("[^a-z0-9]", "");
+        if(!filteredTopic.equals(topic)) return "redirect:/t/" + filteredTopic;
         return "This is the topic site for " + topic;
     }
 
