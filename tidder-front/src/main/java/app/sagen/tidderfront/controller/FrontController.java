e7651a4f6fdfe3f26c4f1badd84b50b2a028ee16
Alexander Sagen <alexmsagen@gmail.com>
2019-04-10 23:05:50 +0200

    Added topic html

--- tidder-front/src/main/java/app/sagen/tidderfront/controller/FrontController.java
+++ tidder-front/src/main/java/app/sagen/tidderfront/controller/FrontController.java
@@ -1,14 +1,25 @@
 package app.sagen.tidderfront.controller;
 
+import app.sagen.tidderfront.model.User;
+import app.sagen.tidderfront.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/t")
 public class FrontController {
+
+    private UserService userService;
+
+    @Autowired
+    FrontController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping()
     public String topic() {
@@ -23,18 +34,17 @@ public class FrontController {
     }
 
     @GetMapping("{topic}")
-    public @ResponseBody String topic(HttpServletResponse response, Model model, @PathVariable("topic") String topic) {
+    public String topic(HttpServletResponse response, Model model, @PathVariable("topic") String topic) {
         String filteredTopic = topic.toLowerCase();
         filteredTopic = filteredTopic.replaceAll("[^a-z0-9]", "");
-        if(!filteredTopic.equals(topic)) {
-            try {
-                response.sendRedirect("/t/" + filteredTopic);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return "";
-        }
-        return "This is the topic site for " + topic;
+        if(!filteredTopic.equals(topic)) return "redirect:/t/" + filteredTopic;
+        Optional<User> optuser = userService.getAuthenticatedUser();
+        User user = null;
+        if(optuser.isPresent()) user = optuser.get();
+        model.addAttribute("currentUser", user);
+        model.addAttribute("topicExists", false);
+        model.addAttribute("topicName", topic);
+        return "topic";
     }
 
 }
