42c650ebb097b41ef7e8ddc8ff11089b83eda270
Alexander Sagen <alexmsagen@gmail.com>
2019-04-22 17:09:50 +0200

    Fixed last commit

---
+++ tidder-likes/src/main/java/app/sagen/tidderlikes/TidderLikesApplication.java
@@ -0,0 +1,13 @@
+package app.sagen.tidderlikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidderLikesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TidderLikesApplication.class, args);
+    }
+
+}
---
+++ tidder-post/src/main/java/app/sagen/tidderpost/TidderPostApplication.java
@@ -0,0 +1,13 @@
+package app.sagen.tidderpost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidderPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TidderPostApplication.class, args);
+    }
+
+}
