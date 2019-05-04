2569a1859eb918c41653a3c55368beb4cdf1c647
Alexander Sagen <alexmsagen@gmail.com>
2019-05-04 15:51:27 +0200

    Added tidder-topic to docker

--- tidder-topic/src/main/java/app/sagen/tiddertopic/TidderTopicApplication.java
+++ tidder-topic/src/main/java/app/sagen/tiddertopic/TidderTopicApplication.java
@@ -2,8 +2,12 @@ package app.sagen.tiddertopic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@Configuration
 public class TidderTopicApplication {
 
     public static void main(String[] args) {
