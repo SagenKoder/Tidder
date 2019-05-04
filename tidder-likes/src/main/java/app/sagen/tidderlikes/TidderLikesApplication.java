f8642ab4fc08407cc9f7a6fae170474825bbc79d
Alexander Sagen <alexmsagen@gmail.com>
2019-05-04 17:03:11 +0200

    Added post and likes to docker

--- tidder-likes/src/main/java/app/sagen/tidderlikes/TidderLikesApplication.java
+++ tidder-likes/src/main/java/app/sagen/tidderlikes/TidderLikesApplication.java
@@ -2,8 +2,12 @@ package app.sagen.tidderlikes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@Configuration
 public class TidderLikesApplication {
 
     public static void main(String[] args) {
--- tidder-post/src/main/java/app/sagen/tidderpost/TidderPostApplication.java
+++ tidder-post/src/main/java/app/sagen/tidderpost/TidderPostApplication.java
@@ -2,8 +2,12 @@ package app.sagen.tidderpost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@Configuration
 public class TidderPostApplication {
 
     public static void main(String[] args) {
