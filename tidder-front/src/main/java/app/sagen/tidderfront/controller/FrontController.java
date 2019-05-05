0ef8fab97a3afc535f890998daeced4c39dd97d9
Alexander Sagen <alexmsagen@gmail.com>
2019-05-05 12:48:58 +0200

    Fixed wrong path

--- tidder-front/src/main/java/app/sagen/tidderfront/controller/FrontController.java
+++ tidder-front/src/main/java/app/sagen/tidderfront/controller/FrontController.java
@@ -40,7 +40,7 @@ public class FrontController {
         return "redirect:/t/" + topic;
     }
 
-    @PostMapping("/createTopic")
+    @PostMapping("/t/createTopic")
     public String createTopic(@RequestParam Topic topic) {
         topic.setName(topic.getName().toLowerCase()); // always lowercase
 
