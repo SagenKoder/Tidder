f34cc011713f8096eefa4251c356472d37688859
Alexander Sagen <alexmsagen@gmail.com>
2019-05-05 17:52:24 +0200

    Added @Entity and @Id to Post entity

--- tidder-post/src/main/java/app/sagen/tidderpost/model/Post.java
+++ tidder-post/src/main/java/app/sagen/tidderpost/model/Post.java
@@ -3,12 +3,16 @@ package app.sagen.tidderpost.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
+@Entity
 public class Post {
 
+    @Id
     private long id;
     private String owner;
     private String topic;
