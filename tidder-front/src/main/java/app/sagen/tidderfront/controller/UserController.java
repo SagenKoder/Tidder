77e8f7a7bccde0983bda9a3f60dae9445fbc39d9
Alexander Sagen <alexmsagen@gmail.com>
2019-05-06 02:19:26 +0200

    Added user controller to front and a user html site

---
+++ tidder-front/src/main/java/app/sagen/tidderfront/controller/UserController.java
@@ -0,0 +1,41 @@
+package app.sagen.tidderfront.controller;
+
+import app.sagen.tidderfront.model.User;
+import app.sagen.tidderfront.service.PostService;
+import app.sagen.tidderfront.service.TopicService;
+import app.sagen.tidderfront.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller("/u")
+public class UserController {
+
+    private UserService userService;
+    private PostService postService;
+    private TopicService topicService;
+
+    @Autowired
+    UserController(UserService userService, PostService postService, TopicService topicService) {
+        this.userService = userService;
+        this.postService = postService;
+        this.topicService = topicService;
+    }
+
+    @GetMapping("{user}")
+    public String userSite(Model model, @PathVariable String username) {
+        Optional<User> userOptional = userService.getAuthenticatedUser();
+        model.addAttribute("currentUser", userOptional.orElseGet(null));
+        Optional<User> selectedUserOpt = userService.findByUsername(username);
+        if(!selectedUserOpt.isPresent()) return "redirect:/t/all";
+        model.addAttribute("user", selectedUserOpt.get());
+        model.addAttribute("posts", postService.fetchPostsByUser(username));
+        return "user";
+    }
+
+}
